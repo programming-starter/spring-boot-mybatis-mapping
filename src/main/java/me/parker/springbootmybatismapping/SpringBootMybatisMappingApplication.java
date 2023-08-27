39337190930c0481cee3941273b3e5e49a56ba43
parker1609 <psbum1609@gmail.com>
2023-08-27 17:16:22 +0900

    init project

---
+++ src/main/java/me/parker/springbootmybatismapping/SpringBootMybatisMappingApplication.java
@@ -0,0 +1,13 @@
+package me.parker.springbootmybatismapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisMappingApplication.class, args);
+    }
+
+}
